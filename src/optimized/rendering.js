54a60c143f5b49e070974e2828559d168074a1bc
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-09-23 16:40:38 -0400

    used performance.now to benchhmark times for different loop methods for same set of data

--- src/optimized/rendering.js
+++ src/optimized/rendering.js
@@ -30,6 +30,13 @@ export function renderProducts(products, deleteProductFn) {
     const newListEl = createElement(product, product.id, deleteProductFn)
     productListEl.appendChild(newListEl);
   });
+  // const startTime = performance.now()
+  // for (let i=0; i < products.length; i++) {
+  //   const newListEl = createElement(products[i], products[i].id, deleteProductFn)
+  //   productListEl.appendChild(newListEl);
+  // }
+  // const endTime = performance.now()
+  // console.log(endTime - startTime)
 }
 
 export function updateProducts(product, prodId, deleteProductFn, isAdding) {
